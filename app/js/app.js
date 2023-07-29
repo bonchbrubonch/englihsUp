0e96716635514aed89ec22421bfa9cdc39985fc4
Artem <nikolloteslla@gmail.com>
2023-07-29 15:25:08 +0300

    add remarks

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,19 @@ $(function () {
 
   $(".header__menu-btn").on("click", function () {
     $(".header__menu").addClass("open");
+    $(".header__menu-wrap").addClass("open");
     $("body").addClass("lock");
   });
 
   $(".header__menu-close").on("click", function () {
     $(".header__menu").removeClass("open");
+    $(".header__menu-wrap").removeClass("open");
+    $("body").removeClass("lock");
+  });
+
+  $(".header__menu-wrap").on("click", function () {
+    $(".header__menu").removeClass("open");
+    $(".header__menu-wrap").removeClass("open");
     $("body").removeClass("lock");
   });
 
