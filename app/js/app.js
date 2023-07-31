e2fc639b38c13556dd9e44ef536ddfeaa0401ce0
Artem <nikolloteslla@gmail.com>
2023-07-31 09:09:53 +0300

    add sections

--- app/js/app.js
+++ app/js/app.js
@@ -12,11 +12,100 @@ $(function () {
     $("body").removeClass("lock");
   });
 
+  $(".reviews__read").on("click", function () {
+    $(this).parent().addClass("open");
+  });
+
   $(".header__menu-shadow").on("click", function () {
     $(".header__menu").removeClass("open");
     $(".header__menu-shadow").removeClass("open");
     $("body").removeClass("lock");
   });
+
+  $('.menu__right-slider').slick({
+    infinite: false,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2.5,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+  $('.reviews__slider').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [{
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+  $('.info__slider').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+
+
+
 
 
 
